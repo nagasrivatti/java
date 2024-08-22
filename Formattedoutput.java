bdc6d5497bbcd5f03e86d8f362119127a71d68b2
nagasrivatti <151708465+nagasrivatti@users.noreply.github.com>
2024-08-22 11:25:03 +0530

    Create Formattedoutput.java

---
+++ Formattedoutput.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class Main
+{
+    public static void main(String args[])
+    {
+    String str="HEMANAGASRI";
+    System.out.printf("The string value is:%s\n",str);
+    int x=512;
+    System.out.printf("\nThe integer value is:%d\n",x);
+    double f=5.25412368;
+    System.out.printf("The decimal value is:%f\n",f);
+    System.out.printf("\nAfter Formatting two specific width of the value is :%4f\n",f);
+    System.out.printf("\nAfter Formattingto right margin the value is %20.4f",f);
+    }
+}
