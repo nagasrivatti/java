628bb2690c22fd12ee2dc33d25f9af058545e610
nagasrivatti <151708465+nagasrivatti@users.noreply.github.com>
2024-08-22 11:23:22 +0530

    Create Staticblock.java

---
+++ Staticblock.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        System.out.println("Inside the main method");
+        
+    }
+    static 
+    {
+        System.out.println("Inside static block");
+        
+    }
+}
