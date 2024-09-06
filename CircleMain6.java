25862a71279906b63400a3b6be0dbdc17e9fac1c
nagasrivatti <151708465+nagasrivatti@users.noreply.github.com>
2024-09-06 16:08:33 +0530

    Update and rename CircleArea7.java to CircleMain6.java

--- CircleMain6.java
+++ CircleMain6.java
@@ -1,13 +1,13 @@
 import java.io.*;
 import java.util.Scanner;
-class Circlearea7
+class CircleArea6
 {
     double r;
-    Circlearea7()
+    CircleArea6()
     {
         r=0;
     }
-    Circlearea7(double r){
+    CircleArea6(double r){
         this.r=r;
     }
     
@@ -22,19 +22,19 @@ class Circlearea7
         this.r=r;
     }
 }
-    class Main
+    class CircleMain6
     {
     
 	public static void main(String[] args) {
 	    Scanner sc=new Scanner(System.in);
-	    Circlearea7 c1=new Circlearea7();
-	    Circlearea7 c2=new Circlearea7(3.6);
-	    Circlearea7 c3=new Circlearea7(5);
+	    CircleArea6 c1=new CircleArea6();
+	    CircleArea6 c2=new CircleArea6(3.6);
+	    CircleArea6 c3=new CircleArea6(5);
 	    System.out.println("Area of 1st circle is "+c1.area());
 	    System.out.println("Area of 1st circle updated is "+c1.area(8));
 	    System.out.println("Enter redius of  circles to find the area:");
 	    double radius=sc.nextDouble();
-	    Circlearea7 c4=new Circlearea7();
+	    CircleArea6 c4=new CircleArea6();
 	    c4.setCircle(radius);
 	    System.out.println("Area of  circle is"+c4.area());
 		System.out.println("Area of 2nd circle is"+c2.area());
