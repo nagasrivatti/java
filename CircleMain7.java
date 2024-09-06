ffa82be7f14575ef5b5b556c40075912ef7eed3a
nagasrivatti <151708465+nagasrivatti@users.noreply.github.com>
2024-09-06 16:12:43 +0530

    Update and rename Circlearea8.java to CircleMain7.java

--- CircleMain7.java
+++ CircleMain7.java
@@ -1,16 +1,16 @@
 import java.io.*;
 import java.util.Scanner;
-class Circlearea8
+class CircleArea7
 {
     double r;
-    Circlearea8()
+    CircleArea7()
     {
         r=0;
     }
-    Circlearea8(double r){
+    CircleArea7(double r){
         this.r=r;
     }
-    Circlearea8(Circlearea8 c){
+    CircleArea7(CircleArea7 c){
         this.r=c.r;
     }
     double area(){
@@ -24,17 +24,17 @@ class Circlearea8
         this.r=r;
     }
 }
-    class Main
+    class CircleMain7
     {
     
 	public static void main(String[] args) {
 	    Scanner sc=new Scanner(System.in);
-	    Circlearea8 c1=new Circlearea8();
+	    CircleArea7 c1=new CircleArea7();
 	    System.out.println("Initially area of 1st circle is "+c1.area());
-	    Circlearea8 c2=new Circlearea8(7.0);
+	    CircleArea7 c2=new CircleArea7(7.0);
 	    System.out.println("Area of circle with radius 7.0 is "+c2.area());
 	    System.out.println("Area of circle with radius 5.5 is "+c2.area(5.5));
-	    Circlearea8 c3=new Circlearea8(c2);
+	    CircleArea7 c3=new CircleArea7(c2);
 	    System.out.println("After copy constructor area of 3rd circle is "+c3.area());
 	    c1.setCircle(8.8);
 	    System.out.println("After set circle area of first circle is "+c1.area());
