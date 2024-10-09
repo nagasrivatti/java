9f3615fab65da936f4127418faa79f0cb409b31f
nagasrivatti <151708465+nagasrivatti@users.noreply.github.com>
2024-10-09 12:18:47 +0530

    Create Superdemo4.java

---
+++ Superdemo4.java
@@ -0,0 +1,36 @@
+import java.io.*;
+class Point2D
+{
+	int x,y;
+	Point2D()
+	{
+	int	x=0;
+	int y=0;
+	}
+	Point2D(int x,int y)
+	{
+		this.x=x;
+		this.y=y;
+	}
+}class Point3D extends Point2D
+{
+	int z;
+	Point3D(){
+		super();
+		z=0;
+	}
+	Point3D(int x,int y,int z)
+	{
+		super(x,y);
+		this.z=z;
+	}
+}
+class Superdemo4
+{
+	public static void main(String args[])
+	{
+		Point3D ob=new Point3D();
+	}
+}
+
+		
